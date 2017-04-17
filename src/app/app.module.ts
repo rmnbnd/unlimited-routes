2e26f80ec0704004a83f37b007c6486a083a3b21
Roman Bondarchuk <rmnbnd@gmail.com>
2017-04-17 20:07:46 +0300

    Add and initialize google map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AgmCoreModule } from 'angular2-google-maps/core'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: ''
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
